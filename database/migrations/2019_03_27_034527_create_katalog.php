dfca4ce0cacc8dd3e6bac207ec7a00dff0b2d145
anandapricillia <anandaapricillia@gmail.com>
2019-03-28 10:06:32 +0700

    migration wallet

---
+++ database/migrations/2019_03_27_034527_create_katalog.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKatalog extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('katalog', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('idKatalog');
+            $table->integer('idKategori');
+            $table->string('pict');
+            $table->string('nama_produk');
+            $table->text('detail');
+            $table->string('harga');
+            $table->string('stok');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('katalog');
+    }
+}
---
+++ database/migrations/2019_03_27_034824_create_order.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrder extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('idOrder');
+            $table->string('jumlah_order');
+            $table->string('harga');
+            $table->string('harga_total');
+            $table->datetime('tanggal_waktu_order');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order');
+    }
+}
